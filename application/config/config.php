6c7032658a5004120f36438b25d146f9ff4bf275
Asim Bilal <a.bilal2@generations.edu.pk>
2018-04-19 15:14:46 -0700

    change config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/parkem/';
+$config['base_url'] = 'http://34.205.32.200/parkem/';
 
 /*
 |--------------------------------------------------------------------------
@@ -401,7 +401,7 @@ $config['sess_regenerate_destroy'] = FALSE;
 
 */
 $config['cookie_prefix']	= '';
-$config['cookie_domain']	= 'localhost';
+$config['cookie_domain']	= '34.205.32.200';
 $config['cookie_path']		= '/';
 $config['cookie_secure']	= FALSE;
 $config['cookie_httponly'] 	= FALSE;
